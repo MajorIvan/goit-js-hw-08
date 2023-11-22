493246774a60b85fb5394babe4fe8ca66f28194f
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-22 17:27:06 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-const localStorage = 'videoplayer-current-time';
+const LS_KEY = 'videoplayer-current-time';
 
-player.setCurrentTime(localStorage.getItem(localStorage) || 0);
+player.setCurrentTime(localStorage.getItem(LS_KEY) || 0);
 
 player.on('timeupdate', throttle(function(data) { 
         if (data && data.seconds) { 
-            localStorage.setItem(localStorage, data.seconds); 
+            localStorage.setItem(LS_KEY, data.seconds); 
         } 
     }
 ), 1000);
