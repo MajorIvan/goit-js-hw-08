fc8a5fe802dd686a157ff34559e78c7bad7015b1
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-22 17:24:23 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,5 @@
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
@@ -5,9 +7,9 @@ const LS_KEY = 'videoplayer-current-time';
 
 player.setCurrentTime(localStorage.getItem(LS_KEY) || 0);
 
-player.on('timeupdate', function(data) { 
+player.on('timeupdate', throttle(function(data) { 
         if (data && data.seconds) { 
             localStorage.setItem(LS_KEY, data.seconds); 
         } 
     }
-);
+), 1000);
