05fdbec614377142895e5c26ab89beaf775c8088
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-22 16:47:06 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const LS_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(LS_KEY) || 0);
+
+player.on('timeupdate', function(data) { 
+        if (data && data.seconds) { 
+            localStorage.setItem(LS_KEY, data.seconds); 
+        } 
+    }
+);
